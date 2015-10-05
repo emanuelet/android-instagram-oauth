76a820ec0450f7ea866d39546ffc41453d88436b
Emanuele Tonello <emanueletonello@gmail.com>
2015-10-05 16:53:05 +1000

    added gradle structure

---
+++ example/src/main/java/etapps/example/MainActivity.java
@@ -0,0 +1,86 @@
+package etapps.example;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import etapps.library.instagram.oauth.InstagramApp;
+
+public class MainActivity extends AppCompatActivity {
+
+    private InstagramApp mApp;
+    private Button btnConnect;
+    private TextView tvSummary;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        mApp = new InstagramApp(this, ApplicationData.CLIENT_ID,
+                ApplicationData.CLIENT_SECRET, ApplicationData.CALLBACK_URL);
+        mApp.setListener(listener);
+
+        tvSummary = (TextView) findViewById(R.id.tvSummary);
+
+        btnConnect = (Button) findViewById(R.id.btnConnect);
+        btnConnect.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                if (mApp.hasAccessToken()) {
+                    final AlertDialog.Builder builder = new AlertDialog.Builder(
+                            MainActivity.this);
+                    builder.setMessage("Disconnect from Instagram?")
+                            .setCancelable(false)
+                            .setPositiveButton("Yes",
+                                    new DialogInterface.OnClickListener() {
+                                        public void onClick(
+                                                DialogInterface dialog, int id) {
+                                            mApp.resetAccessToken();
+                                            btnConnect.setText("Connect");
+                                            tvSummary.setText("Not connected");
+                                        }
+                                    })
+                            .setNegativeButton("No",
+                                    new DialogInterface.OnClickListener() {
+                                        public void onClick(
+                                                DialogInterface dialog, int id) {
+                                            dialog.cancel();
+                                        }
+                                    });
+                    final AlertDialog alert = builder.create();
+                    alert.show();
+                } else {
+                    mApp.authorize();
+                }
+            }
+        });
+
+        if (mApp.hasAccessToken()) {
+            tvSummary.setText("Connected as " + mApp.getUserName());
+            btnConnect.setText("Disconnect");
+        }
+
+    }
+
+    InstagramApp.OAuthAuthenticationListener listener = new InstagramApp.OAuthAuthenticationListener() {
+
+        @Override
+        public void onSuccess() {
+            tvSummary.setText("Connected as " + mApp.getUserName());
+            btnConnect.setText("Disconnect");
+        }
+
+        @Override
+        public void onFail(String error) {
+            Toast.makeText(MainActivity.this, error, Toast.LENGTH_SHORT).show();
+        }
+    };
+}
